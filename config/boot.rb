ef08eeb5484da3415662af2ad14fbbe1d90bd470
Jeanne Keanu Echevarria <keanukho66@gmail.com>
2024-05-01 16:28:29 +0800

    To suppress the warning that I have.

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,11 @@
+# Suppress warnings for this block
+original_verbose = $VERBOSE
+$VERBOSE = nil
+
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+# Restore the original value of $VERBOSE
+$VERBOSE = original_verbose
