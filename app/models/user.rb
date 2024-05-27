411466ee1ea098909d90765eff9507cf824d8f53
keanukho <keanukho66@gmail.com>
2024-05-27 19:29:11 +0800

    Feature 8] User increment children members

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,9 @@ class User < ApplicationRecord
 
   has_many :addresses
 
+  belongs_to :parent, class_name: 'User', optional: true
+  has_many :children, class_name: 'User', foreign_key: 'parent_id'
+
   def valid_phone_number
     unless Phonelib.valid_for_country?(phone_number, 'PH')
       errors.add(:phone_number, "Phone number should be valid.")
